f3c54df94fd21a8ea4f80de9e8b851d852777207
Muondo Vladislav <muondove@gmail.com>
2023-03-20 00:20:58 +0200

    navigating to the movie seat selection screen, passing the correct ID

--- src/components/DayFeature.tsx
+++ src/components/DayFeature.tsx
@@ -7,8 +7,11 @@ import { myGlobalContext } from "./GlobalContextProvider";
 
 function DayFeature(props : {dayID : string, date : string})
 {
+    //IDENTIFIERS for searching data in the context
     const dayFeatureID = props.dayID;
     const dayFeatureDate = props.date;
+
+    //GET info from the context
     const data = useContext(myGlobalContext);
     const currentDay = data.dayEntries.find(entry => entry.dayID === dayFeatureID);
     const movieList = currentDay?.movies;
@@ -16,9 +19,9 @@ function DayFeature(props : {dayID : string, date : string})
     return(
         <div className={styles.dayFeature}>
             <h2 className={styles.dayDate}>{dayFeatureDate}</h2>
-            <section className={styles.movieList}>{movieList?.map(movie => <MovieCard screeningID={movie.screeningID}/>)}</section>
+            <section className={styles.movieList}>{movieList?.map(movie => <MovieCard screeningID = {movie.screeningID}/>)}</section>
         </div>
-    )
+    )   
 }
 
 export default DayFeature;
--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 import styles from "../styles/MovieCard.module.css";
+import { ScreeningID } from "../types/ScreeningID";
 
-function MovieCard(props : any) // fix later
+function MovieCard(props : {screeningID : ScreeningID})
 {
+    const cardID = `${props.screeningID.dayID}::${props.screeningID.movieID}`;
     return(
         // on the line below i ll pas the card id to get specified data
-        <Link to={`/tickets/${props.id}`}>
+        <Link to={`/tickets/${cardID}`}>
             <div className={styles.movieCard}></div>
         </Link>
     )
